bb4c23879c17d75e1ff262ec28f139f7c673fb1f
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-01-28 14:22:53 +0000

    added checkbox and confirmation

--- app.js
+++ app.js
@@ -11,10 +11,30 @@ form.addEventListener('submit', (e)=>{
     input.value = '';
     const li = document.createElement('li');
     li.textContent = text;
+    //creating checkbox input with label
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed ';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    //added checkbox the list item
+    label.appendChild(checkbox);
+    li.appendChild(label);
     ul.appendChild(li);
+});
 
+ul.addEventListener('change', (e)=>{
+    // check if the e.target checkbox is checked or not. true or false
+    console.log(e.target.checked);
+    const checkbox = e.target;
+    const checked  = checkbox.checked;
 
-
+    //getting grandparent of checkbox
+    const parentList = checkbox.parentNode.parentNode;
+    if (checked){
+        parentList.className = 'responded';
+    } else { parentList.className = '';}
+    
 
 
 });
